4ea56283c2c59a7eae000502a5e261d23508fac3
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-20 15:10:56 -0400

    feat: complete 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    // Inputs must be non-negative
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    // Inputs must be integers
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    let sum = 0;
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
