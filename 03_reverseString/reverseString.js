51dae05fa46b5a7f746282887908d3e3112d3898
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-20 14:50:09 -0400

    feat: complete 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    output = ''
+    let arr = str.split('');
+    while (arr.length > 0) {
+        output += arr.pop();
+    }
+    return output;
 };
 
 // Do not edit below this line
