8e8a9c88b1e871a1fcacb29e93472f6223f21fc4
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-20 15:16:49 -0400

    feat: complete 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isYearDivisibleByOneHundred = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
 
+    return isYearDivisibleByFourHundred || isYearDivisibleByFour && !isYearDivisibleByOneHundred;
 };
 
 // Do not edit below this line
